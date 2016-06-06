db26c585f9a51bf9ebc94f1339c79936155f455c
Zachariah Ngonyani <zachariahn@gmail.com>
2016-06-06 07:14:30 +0300

    Fix bank account class to make tests pass

--- lib/bank_account.ex
+++ lib/bank_account.ex
@@ -6,7 +6,8 @@ defmodule BankAccount do
   def await(events) do
     receive do
       {:check_balance, pid} -> divulge_balance(pid, events)
-      {:deposit, amount}    -> events = deposit(amount, events) 
+      {:deposit, amount}    -> events = deposit(amount, events)
+      {:withdraw, amount}   -> events = withdraw(amount, events)
     end 
     await(events) 
   end 
@@ -19,12 +20,29 @@ defmodule BankAccount do
     events ++ [{:deposit, amount}]
   end 
 
+  defp withdraw(amount, events) do
+    events ++ [{:withdrawal, amount}]
+  end 
+
   defp calculate_balance(events) do
-    deposits = sum(events)
-    deposits 
+    deposits    = sum(just_deposits(events))
+    withdrawals = sum(just_withdrawals(events))
+    deposits - withdrawals 
   end 
 
   defp sum(events) do
     Enum.reduce(events, 0, fn{_, amount}, acc -> acc + amount end)
+  end 
+
+  defp just_deposits(events) do
+    just_type(events, :deposit)
+  end 
+
+  defp just_withdrawals(events) do
+    just_type(events, :withdrawal)
+  end 
+
+  defp just_type(events, expected_type) do
+    Enum.filter(events, fn({type, _}) -> type == expected_type end)
   end 
 end
